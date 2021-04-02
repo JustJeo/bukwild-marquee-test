7ed89f601d07cbc46d127a9882c2d1128c4507ec
JustJeo <j.cabrigas.writer@gmail.com>
2021-04-02 03:06:22 -0700

    fixes routes and path issues

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -8,7 +8,7 @@ import AboutUs from '../pages/AboutUs'
 
 const Routes = () => (
   <Switch>
-    <Route exact path = '/' component = { Home } />
+    <Route path = '/' component = { Home } />
     <Route exact path = '/services' component = { Services } />
     <Route exact path = '/aboutus' component = { AboutUs} />
   </Switch>
