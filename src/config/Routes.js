f9d07bdf64b9ac4a87faad8f199a0b331fe9c03c
JustJeo <j.cabrigas.writer@gmail.com>
2021-04-02 03:04:49 -0700

    changes industries to home page

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route} from 'react-router-dom'
 
 // Import all pages
-import Industries from '../pages/Industries'
+import Home from '../pages/Home'
 import Services from '../pages/Services'
 import AboutUs from '../pages/AboutUs'
 
 const Routes = () => (
   <Switch>
-    <Route exact path = '/industries' component = { Industries } />
+    <Route exact path = '/' component = { Home } />
     <Route exact path = '/services' component = { Services } />
     <Route exact path = '/aboutus' component = { AboutUs} />
   </Switch>
